4381b58611ca0705e01c76cb3f61331a056d1b09
Gustavo Leguizamon <gleguizamon472@gmail.com>
2023-02-05 14:27:50 -0300

    Added Super Hero model

---
+++ Super Hero App/Super Hero App/Models/SuperHero.swift
@@ -0,0 +1,21 @@
+//
+//  SuperHero.swift
+//  Super Hero App
+//
+//  Created by Gustavo Leguizamon on 2023-02-05.
+//
+
+import Foundation
+
+struct SuperHero: Identifiable, Decodable {
+    var id: Int
+    var name: String
+    var slug: String
+}
+
+
+
+
+
+
+
