941bca68580e002c4afb3d34b35a3d83e2a77430
Gustavo Leguizamon <gleguizamon472@gmail.com>
2023-02-06 09:06:11 -0300

    Added Pods

---
+++ Pods/Target Support Files/Pods-Super Hero App-Super Hero AppUITests/Pods-Super Hero App-Super Hero AppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Super_Hero_App_Super_Hero_AppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Super_Hero_App_Super_Hero_AppUITests
+@end
